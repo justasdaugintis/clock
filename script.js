24b3eff7e60756ff2fa086ae9c190bab7df56b87
justasdaugintis <justasd101@gmail.com>
2018-02-22 12:12:02 +0000

    Fixed and all working brand new clock
    ma bizzwle

--- script.js
+++ script.js
@@ -197,7 +197,7 @@
 
 
 
-function setDate() {
+function setSeconds() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
@@ -208,18 +208,36 @@ function setDate() {
 //how do you make the seconds hand move
     
     //hour equation
+  
+ 
+ //minute equation
+   
+}
+//pass setDate and we are going to run it every 1 second
+  
+
+
+function setHours() {
+    const now = new Date();
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
  document.querySelector(".hour-hand").style.transform=`rotate(${hoursDegrees}deg)`
-    
- 
- //minute equation
+}
+
+
+
+function setMinutes() {
+    const now = new Date();
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
  document.querySelector(".minute-hand").style.transform=`rotate(${minutesDegrees}deg)`
+
 }
-//pass setDate and we are going to run it every 1 second
-  setInterval(setDate, 1000);
-  setDate();
 
 
+setInterval(setSeconds, 1000);
+setInterval(setHours, 3600000);
+setInterval(setMinutes, 60000);
+  setSeconds();
+  setMinutes();
+  setHours();
